89cecc9ca8f237fe358aafb90ec4d74e9b07a729
Aid3nXR <aidenjiyulkim@gmail.com>
2025-05-25 09:34:11 -0700

    Initialize dependabot/npm_and_yarn/express-5.1.0

--- static/assets/dyn/config.js
+++ static/assets/dyn/config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
 	logLevel: 0,
 	bare: {
 		version: 2,
-		path: "/ca/",
+		path: "/fq/",
 	},
 	tab: {
 		title: null,
@@ -13,12 +13,12 @@ self.__dynamic$config = {
 		ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.3",
 	},
 	assets: {
-		prefix: "/assets/history/",
+		prefix: "/assets/dyn/",
 		files: {
-			handler: "handler.js?v=2025-04-15",
+			handler: "handler.js?v=10-02-2024",
 			client: "client.js?v=12",
 			worker: "worker.js?v=12",
-			config: "config.js?v=2025-04-15",
+			config: "config.js?v=10-02-2024",
 			inject: "",
 		},
 	},
